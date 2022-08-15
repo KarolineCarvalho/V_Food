c68af246aaa87e4bfebaa9b089841fe60b839fa1
Karoline Carvalho <ka.olicarvalho@gmail.com>
2022-08-15 17:35:41 -0300

    Refactor: Add conditional renderization in articleAuthor

--- src/components/molecules/articleAuthor/articleAuthor.jsx
+++ src/components/molecules/articleAuthor/articleAuthor.jsx
@@ -1,12 +1,24 @@
 import styles from "./articleAuthor.module.scss";
+import { Divider } from "@atoms";
 
-const ArticleAuthor = ({ authorImg, articleAuthor, articleDate }) => {
+const ArticleAuthor = ({
+  authorImg,
+  articleAuthor,
+  articleDate,
+  inline = false,
+}) => {
+  const layout = inline === false ? "authorInfo" : "authorInfo--inline";
+  const dividerIsVisible = inline === false ? "divider" : "divider--inline";
+  console.log(layout);
   return (
     <div className={styles.articleAuthorContainer}>
       <img src={authorImg} className={styles.authorImg} />
-      <div>
-        <p className={styles.authorName}>{articleAuthor}</p>
-        <p className={styles.articleDate}>{articleDate}</p>
+      <div className={styles[`${layout}`]}>
+        <p className={styles.authorInfo__authorName}>{articleAuthor}</p>
+        <div className={styles[`authorInfo__${dividerIsVisible}`]}>
+          <Divider />
+        </div>
+        <p className={styles.authorInfo__articleDate}>{articleDate}</p>
       </div>
     </div>
   );
