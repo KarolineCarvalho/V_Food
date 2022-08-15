8c2a58b5b426cc21eef7b80854aab6a461c4ec8b
Karoline Carvalho <ka.olicarvalho@gmail.com>
2022-08-15 16:00:16 -0300

    Fix: Social media img size

--- src/components/molecules/socialImg/socialImg.jsx
+++ src/components/molecules/socialImg/socialImg.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./socialImg.module.scss";
 
 const SocialImg = ({ children }) => {
-  return <svg className={styles.Svg}  viewBox="0 0 47 36">
-    <a className={styles.Link} href="#">
-      {children}
-    </a>
-  </svg>
-}
+  return (
+    <svg className={styles.Svg} width="30" height="30" viewBox="0 0 47 36">
+      <a className={styles.Link} href="#">
+        {children}
+      </a>
+    </svg>
+  );
+};
 
 export default SocialImg;
