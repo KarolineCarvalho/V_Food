25b790d95d05a9ef5ab2d664969f5d07041cf57a
Karoline Carvalho <ka.olicarvalho@gmail.com>
2022-08-16 10:52:53 -0300

    Add AboutUsQnA to AboutUs page

--- src/components/molecules/AboutUsQnA/AboutUsQnA.jsx
+++ src/components/molecules/AboutUsQnA/AboutUsQnA.jsx
@@ -1,7 +1,67 @@
 import styles from "./AboutUsQnA.module.scss";
+import womanCooking from "/images/womanCooking.png";
+import { Heading } from "@atoms";
 
 const AboutUsQnA = () => {
-  return <div>oi</div>;
+  return (
+    <div>
+      <p className={styles.question}>
+        How did you start out in the food industry?
+      </p>
+      <p className={styles.answer}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+        ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+        sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. Fusce id
+        mollis ex. Praesent feugiat elementum ex ut suscipit.
+      </p>
+
+      <p className={styles.question}>What do you like most about your job?</p>
+      <p className={styles.answer}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+        ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+        sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. Fusce id
+        mollis ex. Praesent feugiat elementum ex ut suscipit.
+      </p>
+
+      <p className={styles.question}>Do you cook at home on your days off?</p>
+      <img src={womanCooking} alt="Woman Cooking" className={styles.img} />
+      <p className={styles.answer}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+        ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+        sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. Fusce id
+        mollis ex. Praesent feugiat elementum ex ut suscipit.
+      </p>
+
+      <p className={styles.question}>
+        What would your advice be to young people looking to get their foot in
+        the door?
+      </p>
+      <p className={styles.answer}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+        ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+        sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. Fusce id
+        mollis ex. Praesent feugiat elementum ex ut suscipit.
+      </p>
+
+      <div className={styles.quote}>
+        <p className={styles.quote__text}>
+          “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+          ultrices odio.”
+        </p>
+      </div>
+
+      <p className={styles.question}>
+        What is the biggest misconception that people have about being a
+        professional chef?
+      </p>
+      <p className={styles.answer}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+        ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+        sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. Fusce id
+        mollis ex. Praesent feugiat elementum ex ut suscipit.
+      </p>
+    </div>
+  );
 };
 
 export default AboutUsQnA;
--- src/components/organisms/AboutUsContent/AboutUsContent.jsx
+++ src/components/organisms/AboutUsContent/AboutUsContent.jsx
@@ -1,10 +1,38 @@
 import styles from "./AboutUsContent.module.scss";
 import { Heading } from "@atoms";
+import { ArticleAuthor, SocialMedia, AboutUsQnA } from "@molecules";
+import authorImg from "/images/JohnSmith.png";
+import cookingPan from "/images/cooking-pan-fire.png";
 
 const AboutUsContent = () => {
   return (
-    <div>
-      <Heading>Full Guide to Becoming a Professional Chef</Heading>
+    <div className={styles.aboutUs}>
+      <div className={styles.aboutUs__title}>
+        <Heading>Full Guide to Becoming a Professional Chef</Heading>
+      </div>
+      <div className={styles.aboutUs__author}>
+        <ArticleAuthor
+          className="authorContainer"
+          authorImg={authorImg}
+          articleAuthor="John Smith"
+          articleDate="15 March 2022"
+          inline={true}
+        />
+      </div>
+
+      <p className={styles.aboutUs__description}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+        ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+        sed velit a faucibus. In feugiat vestibulum velit vel pulvinar.
+      </p>
+      <img src={cookingPan} className={styles.aboutUs__img} />
+      <div className={styles.aboutUs__socialMedia}>
+        <p>SHARE THIS ON:</p>
+        <SocialMedia class={"--vertical"} />
+      </div>
+      <div className={styles.aboutUs__QnA}>
+        <AboutUsQnA />
+      </div>
     </div>
   );
 };
